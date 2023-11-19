b75e6cc83906b2c9a43dfdd8d22f5b4343b5a058
sebhall <sebhalldesigns@gmail.com>
2023-11-19 14:54:09 +0000

    clean slate refactor

--- geomod_demo/shaders/basic.fs
+++
@@ -1,7 +0,0 @@
-#version 330
-out vec4 FragColor;
-
-void main() {
-    
-    FragColor = vec4(0.75, 0.75, 1.0, 0.75);
-}
--- geomod_demo/src/renderer/mod.rs
+++
@@ -1,8 +0,0 @@
-mod buffer;
-mod program;
-mod renderer;
-mod shader;
-mod texture;
-mod vertex_array;
-
-pub use renderer::Renderer;
